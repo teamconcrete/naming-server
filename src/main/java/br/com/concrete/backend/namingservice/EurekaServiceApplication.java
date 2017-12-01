85bbf9c6921211739851bcc45d3416065ed9b890
“Assis <francisco.melo@concrete.com.br>
2017-12-01 16:37:38 -0200

    initial commit

---
+++ src/main/java/br/com/concrete/backend/namingservice/EurekaServiceApplication.java
@@ -0,0 +1,14 @@
+package br.com.concrete.backend.namingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServiceApplication.class, args);
+	}
+}
